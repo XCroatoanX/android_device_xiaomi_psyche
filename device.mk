68d8bf3b0e5358bbc0d278acad71b3a1b3802081
XCroatoanX <stasyukartem2@gmail.com>
2023-11-27 14:45:45 +0100

    Removed unneeded package

--- device.mk
+++ device.mk
@@ -93,10 +93,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
-# Dolby
-PRODUCT_PACKAGES += \
-    DolbyManager
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
